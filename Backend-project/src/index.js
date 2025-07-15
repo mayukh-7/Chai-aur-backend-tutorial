2cc38324eddeb5b8ddcb95305aaf86212f67cd0b
mayukh-7 <mayukhmaity7@gmail.com>
2025-07-15 17:11:00 +0530

    connection to mongoDB with Debugging

---
+++ Backend-project/src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path: './env'}) // this approach is also correct 
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is our first approach to connect DataBase but it is not professional using iffe '()()'
+/*
+import express from "express";
+const app = express();
+;(async() => {
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", (error)=>{
+        console.log("ERRR: ", error);
+        throw error
+       })
+       app.listen(process.env.PORT, ()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+       })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+
+*/
