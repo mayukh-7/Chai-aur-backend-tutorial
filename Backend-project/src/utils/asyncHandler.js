bbd1ec2d8179d5ee095bbb1fa8afc32115fcc24d
mayukh-7 <mayukhmaity7@gmail.com>
2025-07-20 23:06:59 +0530

    complete guide of routers and controller with debugging

--- Backend-project/src/utils/asyncHandler.js
+++ Backend-project/src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //This one way to represent wrapper function . These functions would be used a lot in our project thats why we inserted this in our utilities folder
 //This is represented in promise format
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+   return (req, res, next) => {
         Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
     }
 }
