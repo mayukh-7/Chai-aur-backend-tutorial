29386c207464082a82cf05539c341cb891bf2010
mayukh-7 <mayukhmaity7@gmail.com>
2025-07-25 00:21:17 +0530

    Access Refresh Token, Middleware and cookies in Backend part 2

--- Backend-project/src/controllers/user.controller.js
+++ Backend-project/src/controllers/user.controller.js
@@ -148,10 +148,32 @@ const loginUser = asyncHandler( async (req,res)=>{
 })
 
 const logoutUser = asyncHandler(async(req,res)=>{
-    User.findById
+   await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken : undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly : true,
+        secure: true
+   }
+
+   return res
+   .status(200)
+   .clearCookie("accessToken", options)
+   .clearCookie("refreshToken",options)
+   .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 } 
