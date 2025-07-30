598f2007113fa75e4e6a9d375eccddbbf76cd582
mayukh-7 <mayukhmaity7@gmail.com>
2025-07-31 00:01:58 +0530

    Summary of Backend series

--- Backend-project/src/routes/user.routes.js
+++ Backend-project/src/routes/user.routes.js
@@ -27,7 +27,7 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 
 // In this route we have to give the same name that we have taken while destructuring because we are taking the data from req.params
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
